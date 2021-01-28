9d687b96399181dc3985336919faade2993721b5
cubercsl <2014cais01@gmail.com>
2021-01-28 20:31:50 +0800

    feat: archlinux conda init

--- .config/fish/custom/conf.d/conda_init.fish
+++ .config/fish/custom/conf.d/conda_init.fish
@@ -1,1 +1,10 @@
-eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+switch (uname)
+case Linux
+    # ArchLinux install Anaconda at /opt
+    set CONDA_ROOT /opt/anaconda
+case Darwin
+    # Homebrew install Anaconda at /usr/local
+    set CONDA_ROOT /usr/local/anaconda3
+end
+
+eval $CONDA_ROOT/bin/conda "shell.fish" "hook" $argv | source
--- .config/fish/custom/conf.d/custom_alias.fish
+++
@@ -1,6 +0,0 @@
-switch (uname)
-case Darwin
-    alias gcc=gcc-10
-    alias g++=g++10
-case Linux
-end
