80a4cdb7acbd1cc70e65196ff157869fc766af9f
cubercsl <2014cais01@gmail.com>
2021-02-09 16:03:59 +0800

    fix(i3lock, fish): some small changes
    - drop i3lock, use lightdm-lock

--- .config/fish/custom/functions/fish_title.fish
+++ .config/fish/custom/functions/fish_title.fish
@@ -1,2 +1,8 @@
 function fish_title
+    # emacs is basically the only term that can't handle it.
+    if not set -q INSIDE_EMACS
+        set realhome ~
+        set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+        echo (whoami)@(prompt_hostname): $tmp
+    end
 end
