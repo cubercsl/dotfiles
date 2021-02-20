fdd94dec858ff2413af321c635a81da6931e2b08
cubercsl <2014cais01@gmail.com>
2021-02-20 17:38:21 +0800

    fix(bootstrap): change bootstrap scripts

--- .config/yadm/bootstrap.d/90-fish-plug.sh
+++ .config/yadm/bootstrap.d/90-fish-plug.sh
@@ -4,11 +4,11 @@ FISH_CONFIG_PATH=${FISH_CONFIG_PATH:-$HOME/.config/fish}
 
 if [ -d "$FISH_CONFIG_PATH/custom/scripts" ]; then
   echo "Bootstraping Fish"
-  for i in $FISH_CONFIG_PATH/custom/scripts/*.fish; do
-    if [ -r $i ]; then
-      fish $i
+  find "$FISH_CONFIG_PATH/custom/scripts" -type f | sort | while IFS= read -r script; do
+    if [[ -x "$script" && ! "$script" =~ "##" && ! "$script" =~ "~$" ]]; then
+      if ! "$script"; then
+        echo "Warning: $script didn't return 0" >&2
+      fi
     fi
   done
-  unset i
 fi
-
