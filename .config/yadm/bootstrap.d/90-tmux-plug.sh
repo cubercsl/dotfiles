d1c04bb95c8ae9e66c2ba65c165a2afa0ee1b166
cubercsl <2014cais01@gmail.com>
2021-03-02 22:33:19 +0800

    fix: tmux bootstrap

--- .config/yadm/bootstrap.d/90-tmux-plug.sh
+++ .config/yadm/bootstrap.d/90-tmux-plug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 system_type=$(uname -s)
-[ system_type != "Linux" ] && exit
-[ -d ~/.config/tmux/plugin/tpm ] && git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+[ $system_type != "Linux" ] && exit
+[ ! -d ~/.config/tmux/plugins ] && git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 tmux -c ~/.config/tmux/plugins/tpm/bindings/install_plugins
