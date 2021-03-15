5524c1cda5dfd0391bab124e8e87bf9f6756e6de
cubercsl <2014cais01@gmail.com>
2021-03-15 18:51:39 +0800

    fix: change fish color

--- .config/fish/custom/scripts/config_fish_colors.fish
+++ .config/fish/custom/scripts/config_fish_colors.fish
@@ -4,26 +4,31 @@
 
 # Test:
 # echo "quote" > redirection;;; error param $operator \x42 # comment
-set -U fish_color_command blue -o
-set -U fish_color_quote green
-set -U fish_color_redirection magenta
-set -U fish_color_end cyan
-set -U fish_color_error red
-set -U fish_color_param yellow
-set -U fish_color_comment brblack
-set -U fish_color_operator magenta
-set -U fish_color_escape cyan
-set -U fish_color_autosuggestion brblack
+set -U fish_color_autosuggestion '555'  'brblack'
+set -U fish_color_cancel -r
+set -U fish_color_command --bold
+set -U fish_color_comment red
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end brmagenta
+set -U fish_color_error brred
+set -U fish_color_escape 'bryellow'  '--bold'
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator bryellow
+set -U fish_color_param cyan
+set -U fish_color_quote yellow
+set -U fish_color_redirection brblue
+set -U fish_color_search_match 'bryellow'  '--background=brblack'
+set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+
 
 # Test:
 # commandline --insert 'set ' && commandline --function complete
 set -U fish_pager_color_progress black --background=white
 set -U fish_pager_color_prefix --bold
 set -U fish_pager_color_description yellow
-
-# Remove variables for default prompt
-# fish_color_match is deprecated in Fish 3.2
-for var in cwd cwd_root user host host_remote status match
-    set -e fish_color_$var
-end
-
