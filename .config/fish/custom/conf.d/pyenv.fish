e788abf75a31367be0c554e96c01bacfff49aa36
cubercsl <2014cais01@gmail.com>
2022-01-01 20:09:47 +0800

    fix pyenv

--- .config/fish/custom/conf.d/pyenv.fish
+++ .config/fish/custom/conf.d/pyenv.fish
@@ -1,3 +1,4 @@
+command -sq pyenv || exit
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 status --is-interactive; and pyenv virtualenv-init - | source
