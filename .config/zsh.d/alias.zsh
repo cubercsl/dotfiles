175bed6c794e05cfce423c8c5fc6607410c0417c
cubercsl <2014cais01@gmail.com>
2022-05-11 21:09:56 +0800

    feat: use lsd as ls

--- .config/zsh.d/alias.zsh
+++ .config/zsh.d/alias.zsh
@@ -1,18 +1,17 @@
 # reload zsh
 alias reload="sync && source $HOME/.zshrc && rehash"
 
-alias ls='ls --color=auto --human-readable --time-style=long-iso --hyperlink=auto'
-
+(( $+commands[lsd] )) && alias ls='lsd'
 alias l='ls -al'         # Lists in one column, hidden files.
 alias ll='ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
 alias la='ll -A'         # Lists human readable sizes, hidden files.
 alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
+alias lx='ll -X'         # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+#alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
+#alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 
 # bat using noexpandtab
 (( $+commands[bat] )) && export BAT_OPTS="--tabs 0"
