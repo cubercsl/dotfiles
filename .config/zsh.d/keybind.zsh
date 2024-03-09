383fe28682aa8a49da185cf947b24dc9dfd3a032
cubercsl <2014cais01@gmail.com>
2024-03-09 22:11:18 +0800

    update

--- .config/zsh.d/keybind.zsh
+++ .config/zsh.d/keybind.zsh
@@ -1,11 +1,13 @@
 # use emacs keybind
+autoload -Uz terminfo
+
 bindkey -e
 
-# https://wiki.archlinux.org/title/Zsh#Key_bindings
-# create a zkbd compatible hash;
-# to add other keys to this hash, see: man 5 terminfo
-typeset -g -A key
-key=(
+typeset -A key=(
+    Esc         '\e'
+    Space       ' '
+    Ctrl+Space  '^ '
+    Alt+Space   '\e '
     Home        "${terminfo[khome]}"
     End         "${terminfo[kend]}"
     Insert      "${terminfo[kich1]}"
@@ -17,8 +19,63 @@ key=(
     Right       "${terminfo[kcuf1]}"
     PageUp      "${terminfo[kpp]}"
     PageDown    "${terminfo[knp]}"
+    Tab         "${terminfo[ht]}"
     Shift-Tab   "${terminfo[kcbt]}"
+    Enter       "${terminfo[kent]}"
+    Options     "${terminfo[kopt]}"
+    F1          "${terminfo[kf0]}"
+    F2          "${terminfo[kf0]}"
+    F3          "${terminfo[kf0]}"
+    F4          "${terminfo[kf0]}"
+    F5          "${terminfo[kf0]}"
+    F6          "${terminfo[kf0]}"
+    F7          "${terminfo[kf0]}"
+    F8          "${terminfo[kf0]}"
+    F9          "${terminfo[kf0]}"
+    F10         "${terminfo[kf0]}"
+    F11         "${terminfo[kf0]}"
+    F12         "${terminfo[kf0]}"
 )
+
+# these are from terminfo user_cap (man user_caps)
+local -A _mods=(
+    Shift               '2'
+    Alt                 '3'
+    Alt-Shift           '4'
+    Ctrl                '5'
+    Ctrl-Shift          '6'
+    Ctrl-Alt            '7'
+    Ctrl-Alt-Shift      '8'
+    Meta                '9'
+    Meta-Shift          '10'
+    Meta-Alt            '11'
+    Meta-Alt-Shift      '12'
+    Meta-Ctrl           '13'
+    Meta-Ctrl-Shift     '14'
+    Meta-Ctrl-Alt       '15'
+    Meta-Ctrl-Alt-Shift '16'
+)
+
+for k v in ${(kv)_mods}; do
+    key[${k}-Up]="${terminfo[kUP${v}]}"
+    key[${k}-Down]="${terminfo[kDN${v}]}"
+    key[${k}-Left]="${terminfo[kLFT${v}]}"
+    key[${k}-Right]="${terminfo[kRIT${v}]}"
+    key[${k}-Home]="${terminfo[kHOM${v}]}"
+    key[${k}-End]="${terminfo[kEND${v}]}"
+    key[${k}-PageUp]="${terminfo[kPRV${v}]}"
+    key[${k}-PageDown]="${terminfo[kNXT${v}]}"
+    key[${k}-Delete]="${terminfo[kDC${v}]}"
+    key[${k}-Insert]="${terminfo[kIC${v}]}"
+done
+
+for k in {A..Z} {0..9} '=' '*' '+' '-' '.' '/'; do
+    key[${k:u}]="${k:u}"
+    key[Ctrl-${k:u}]='^'"${k:u}"
+    key[Alt-${k:u}]='\e'"${k:l}"
+    key[Ctrl-Alt-${k:u}]='\e'"[6${k:u}"
+    key[Alt-Shift-${k:u}]='\e'"${k:u}"
+done
 
 # setup key accordingly
 [[ -n "${key[Home]}"      ]] && bindkey -- "${key[Home]}"       beginning-of-line
@@ -45,22 +102,18 @@ if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
 fi
 
 # [Ctrl-Delete] - delete whole forward-word
-bindkey -- '^[[3;5~' kill-word
+bindkey -- "$key[Ctrl-Delete]" kill-word
 # [Ctrl-RightArrow] - move forward one word
-bindkey -- '^[[1;5C' forward-word
+bindkey -- "$key[Ctrl-Right]" forward-word
 # [Ctrl-LeftArrow] - move backward one word
-bindkey -- '^[[1;5D' backward-word
+bindkey -- "$key[Ctrl-Left]" backward-word
 
-bindkey -- '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
-bindkey -s '\el' 'ls\n'                                  # [Esc-l] - run command: ls
-bindkey -- '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-bindkey -- ' ' magic-space                               # [Space] - don't do history expansion
+bindkey -- "$key[Space]" magic-space                                # [Space] - don't do history expansion
 
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey -- '\C-x\C-e' edit-command-line
-
+bindkey -- "$key[Ctrl-X]$key[Ctrl-E]" edit-command-line
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
 
@@ -72,12 +125,6 @@ sudo-command-line() {
 }
 zle -N sudo-command-line
 # [Esc] [Esc]
-bindkey -- '\e\e' sudo-command-line
-
-if (( $+commands[fzf] )); then
-    source /usr/local/share/fzf/key-bindings.zsh 2>/dev/null || \
-    source /usr/share/fzf/key-bindings.zsh 2>/dev/null
-fi
-
+bindkey -- "$key[Esc]$key[Esc]" sudo-command-line
 
 # vim: ft=zsh sw=4 ts=8 sts=4 et:
